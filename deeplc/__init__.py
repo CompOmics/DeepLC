b4451d079acd5c823ece962c327ca1c45e82c148
ArthurDeclercq <arre_declercq@hotmail.com>
2026-01-08 14:01:43 +0100

    add finetune to import

--- deeplc/__init__.py
+++ deeplc/__init__.py
@@ -2,11 +2,12 @@
 
 from importlib.metadata import version
 
-from deeplc.core import calibrate_and_predict, finetune_and_predict, predict
+from deeplc.core import calibrate_and_predict, finetune, finetune_and_predict, predict
 
 __version__: str = version("deeplc")
 __all__: list[str] = [
     "predict",
     "calibrate_and_predict",
     "finetune_and_predict",
+    "finetune",
 ]
